a2ea70b48129ac6d30cf74ccad25d4ea5e1db594
yujx <yujx@dist.com.cn>
2019-11-26 12:58:51 +0800

    需要在使用配置变量的类上加上@RefreshScope注解才会更新

--- licensing-service/src/main/java/cn/x5456/lincenses/config/ServiceConfig.java
+++ licensing-service/src/main/java/cn/x5456/lincenses/config/ServiceConfig.java
@@ -1,8 +1,10 @@
 package cn.x5456.lincenses.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
+@RefreshScope
 @Component
 public class ServiceConfig {
 
